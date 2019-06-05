8fe9945af8c6f82aa997719dacf01c04d6c884e8
Niki Foteinopoulou <niki.foteinopolou@uk.ey.com>
2019-06-05 14:23:04 +0100

    updated with local host

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-export MLFLOW_TRACKING_URI=https://westeurope.azuredatabricks.net/?o=6802933195053587
+export MLFLOW_TRACKING_URI=https://westeurope.azuredatabricks.net
+export MLFLOW_TRACKING_USERNAME="GB012035348-MSP01"
+export MLFLOW_TRACKING_PASSWORD="YSyn3%~9@GjohWr"
 mlflow run https://github.com/NickyFot/hello_databricks.git  -c cluster-spec.json --experiment-id 362698882086974
