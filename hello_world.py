629dfd0272a7a1d902ee983491b60a12a57d932e
Niki Foteinopoulou <niki.foteinopolou@uk.ey.com>
2019-06-10 11:42:03 +0100

    added test parameter

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,25 @@
 import mlflow
+import click
 
-# print("I am printing Hello world!")
-with mlflow.start_run() as run:
-    # Log a parameter (key-value pair)
-    mlflow.log_param("param1", 5)
 
-    # Log a metric; metrics can be updated throughout the run
-    mlflow.log_metric("foo", 1)
-    mlflow.log_metric("foo", 2)
-    mlflow.log_metric("foo", 3)
+@click.command(help="Give a name to the hello app")
+@click.option("--your-name", default="Unidentified",
+              help="Limit the data size to run comfortably on a laptop.")
+def hello_databricks(name):
+    with mlflow.start_run() as run:
+        # Log a parameter (key-value pair)
+        mlflow.log_param("param1", 5)
 
-    # Log an artifact (output file)
-    with open("output.txt", "w") as f:
-        f.write("I am logging Hello world!")
-    mlflow.log_artifact("output.txt")
+        # Log a metric; metrics can be updated throughout the run
+        mlflow.log_metric("foo", 1)
+        mlflow.log_metric("foo", 2)
+        mlflow.log_metric("foo", 3)
+
+        # Log an artifact (output file)
+        with open("output.txt", "w") as f:
+            f.write("I am logging Hello world! Hello {}".format(name))
+        mlflow.log_artifact("output.txt")
+
+
+if __name__=="__main__":
+    hello_databricks()
